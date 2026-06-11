62027bfbcb8ab0f56a72d146ef9e04fa7f4bc039
Gr3yPh <lzx20100624@outlook.com>
2026-06-11 13:34:48 +0800

    renewed kitty theme and nvim colorscheme, also hyprland config

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,10 +1,15 @@
 return {
-  { "paul-han-gh/tomorrow.nvim" },
-
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tomorrow-night",
-    },
+    "Shatur/neovim-ayu",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.o.background = "light" -- 确保 background 是 dark
+      require("ayu").setup({
+        mirage = false, -- 改为 true 使用 mirage 变体
+        terminal = true,
+      })
+      vim.cmd("colorscheme ayu")
+    end,
   },
 }
